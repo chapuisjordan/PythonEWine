c2bb982cfaf3b090dc26f7cb4480432c0c5e5898
dan0909@hotmail.fr <dan0909@hotmail.fr>
2020-01-28 14:59:58 +0100

    call file

--- ewine/script.py
+++ ewine/script.py
@@ -8,8 +8,7 @@ import websockets
 # # insert at 1, 0 is the script path (or '' in REPL)
 # sys.path.insert(1, 'Freenove_RFID_Starter_Kit_for_Raspberry_Pi/Code/Python_Code/Blink')
 
-import file
-from led import Blink
+from led import setup, loop, destroy
 
 connected = set()
 async def pub_sub(websocket, path):
@@ -36,41 +35,16 @@ async def pub_sub(websocket, path):
         except:
             print("WRITER "+str(websocket.remote_address)+" disconnected")
 
-async def trigger_method(method):
-    if method == "takeBottle" :
+async def trigger_method(message):
+    print(message)
+    y = json.loads(message)
+    if y.method == "takeBottle" :
         import RPi.GPIO as GPIO
         import time
-
-        #ledPin = 33    # LedPin for GPIO 13
-        ledPin = 31 # LedPin for GPIO 17
-        #ledPin = 35 # LedPin for GPIO 19
-        #ledPin = 40
-
-        def setup():
-            GPIO.setmode(GPIO.BOARD)       # use PHYSICAL GPIO Numbering
-            GPIO.setup(ledPin, GPIO.OUT)   # set the ledPin to OUTPUT mode
-            GPIO.output(ledPin, GPIO.LOW)  # make ledPin output LOW level 
-            print ('using pin%d'%ledPin)
-
-        def loop():
-            while True:
-                GPIO.output(ledPin, GPIO.HIGH)  # make ledPin output HIGH level to turn on led
-                print ('led turned on >>>')     # print information on terminal
-                time.sleep(1)                   # Wait for 1 second
-                GPIO.output(ledPin, GPIO.LOW)   # make ledPin output LOW level to turn off led
-                print ('led turned off <<<')
-                time.sleep(1)                   # Wait for 1 second
-
-        def destroy():
-            GPIO.cleanup()                      # Release all GPIO
-
-        if __name__ == '__main__':    # Program entrance
-            print ('Program is starting ... \n')
-            setup()
-            try:
-                loop()
-            except KeyboardInterrupt:   # Press ctrl-c to end the program.
-                destroy()
+        ledPin = y.ledPin
+        setup()
+        loop()
+    if 
 
 start_server = websockets.serve(pub_sub, '192.168.1.26', 5678)
 asyncio.get_event_loop().run_until_complete(start_server)
