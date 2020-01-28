0f6e03ed5325ed358d4e39cbe06dae23a535062c
chapuisjordan <dan0909@hotmail.fr>
2020-01-28 15:59:42 +0100

    add temperature / humidity scripts

--- Code/Python_Code/21.1.1_DHT11/DHT11.py
+++ Code/Python_Code/21.1.1_DHT11/DHT11.py
@@ -12,7 +12,7 @@ DHTPin = 11     #define the pin of DHT11
 
 def loop():
     dht = DHT.DHT(DHTPin)   #create a DHT class object
-    sumCnt = 0              #number of reading times 
+    sumCnt = 0              #number of reading times
     while(True):
         sumCnt += 1         #counting number of reading times
         chk = dht.readDHT11()     #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
@@ -25,15 +25,15 @@ def loop():
             print("DHTLIB_ERROR_TIMEOUT!")
         else:               #other errors
             print("Other error!")
-            
+
         print("Humidity : %.2f, \t Temperature : %.2f \n"%(dht.humidity,dht.temperature))
-        time.sleep(2)       
-        
+        time.sleep(10)
+
 if __name__ == '__main__':
     print ('Program is starting ... ')
     try:
         loop()
     except KeyboardInterrupt:
         GPIO.cleanup()
-        exit()  
+        exit()
 
