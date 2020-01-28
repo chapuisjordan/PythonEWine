ce32e0818e598b35b9e2225297a4cd368b2009ee
dan0909@hotmail.fr <dan0909@hotmail.fr>
2020-01-28 16:01:08 +0100

    add websocket js front LCD

---
+++ ewine/script.js
@@ -0,0 +1,17 @@
+const temperature = document.getElementsByClassName("temperature");
+// const humidite = document.getElementsByClassName("humidite");
+
+
+// temperature[0].appendChild("test")
+// humidite.appendChild("testDeux")
+let socket = new WebSocket("wss://192.168.1.126:5678");
+
+exampleSocket.onmessage = function (event) {
+    console.log(event.data);
+}
+
+window.onload = function(){
+    // your JS here
+    console.log(temperature[0])
+    temperature[0].append();
+}
---
+++ ewine/temperature.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+#############################################################################
+# Filename    : DHT11.py
+# Description :	read the temperature and humidity data of DHT11
+# Author      : freenove
+# modification: 2018/08/03
+########################################################################
+import RPi.GPIO as GPIO
+import time
+import Freenove_DHT as DHT
+DHTPin = 11     #define the pin of DHT11
+
+def loop():
+    dht = DHT.DHT(DHTPin)   #create a DHT class object
+    sumCnt = 0              #number of reading times 
+    while(True):
+        sumCnt += 1         #counting number of reading times
+        chk = dht.readDHT11()     #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
+        print ("The sumCnt is : %d, \t chk    : %d"%(sumCnt,chk))
+        if (chk is dht.DHTLIB_OK):      #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
+            print("DHT11,OK!")
+            return [dht.humidity, dht.temperature]
+        elif(chk is dht.DHTLIB_ERROR_CHECKSUM): #data check has errors
+            print("DHTLIB_ERROR_CHECKSUM!!")
+            return "error"
+        elif(chk is dht.DHTLIB_ERROR_TIMEOUT):  #reading DHT times out
+            print("DHTLIB_ERROR_TIMEOUT!")
+            return "error"
+        else:             
+            print("error")
+            
+        print("Humidity : %.2f, \t Temperature : %.2f \n"%(dht.humidity,dht.temperature))
+        time.sleep(60)
+        
+if __name__ == '__main__':
+    print ('Program is starting ... ')
+    try:
+        loop()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        exit()  
+
