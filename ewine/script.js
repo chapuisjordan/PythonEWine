ef2bebc66d3cd61419e69175b526ef2cadd128f8
dan0909@hotmail.fr <dan0909@hotmail.fr>
2020-01-29 15:35:03 +0100

    js modif

--- ewine/script.js
+++ ewine/script.js
@@ -6,6 +6,10 @@
 // humidite.appendChild("testDeux")
 let socket = new WebSocket("ws://192.168.1.27:5678/broadcast/write");
 
+socket.onopen = function (event) {
+    socket.send("Voici un texte que le serveur attend de recevoir dès que possible !"); 
+};
+
 socket.onmessage = function (event) {
    console.log("test")
    console.log(event.data);
