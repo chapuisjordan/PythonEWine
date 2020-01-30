65e69c4cbe2defe6026398135b0b44a27bd4f5ea
dan0909@hotmail.fr <dan0909@hotmail.fr>
2020-01-30 10:01:04 +0100

    archi

--- ewine/front/script.js
+++ ewine/front/script.js
@@ -1,7 +1,7 @@
 // const WebSocket = require('ws');
 
 let socket = new WebSocket("ws://192.168.1.28:5678/broadcast/temperature/read");
-
+console.log('test')
 socket.onmessage = function (event) {
 	console.log('test')
 	console.log(event.data)
