be5c0b43a420a350df31b149dd8c2404961b9441
dan0909@hotmail.fr <dan0909@hotmail.fr>
2020-01-30 09:47:55 +0100

    modif script

--- ewine/script.js
+++ ewine/script.js
@@ -1,39 +1,8 @@
-<<<<<<< Updated upstream
-var WebSocketServer = require("ws").Server;
-var ws = new WebSocketServer( { port: 8100 } );
+// const WebSocket = require('ws');
 
-console.log("Server started...");
-console.log("ws : ", ws)
-ws.on('connection', function (ws) {
-  console.log("Browser connected online...")
-   
-  ws.on("message", function (str) {
-     var ob = JSON.parse(str);
-     switch(ob.type) {
-     case 'text':
-         console.log("Received: " + ob.content)
-         ws.send('{ "type":"text", "content":"Server ready."}')
-         break;
-     case 'image':
-         console.log("Received: " + ob.content)         
-         console.log("Here is an apricot...")
-         var path ="apricot.jpg";   
-         var data = '{ "type":"image", "path":"' + path + '"}';
-         ws.send(data); 
-         break;
-      }   
-    })
-=======
-// const temperature = document.getElementsByClassName("temperature");
-// const humidite = document.getElementsByClassName("humidite");
-// const WebSocket = require('ws')
+let socket = new WebSocket("ws://192.168.1.28:5678/broadcast/temperature/read");
 
-// temperature[0].appendChild("test")
-// humidite.appendChild("testDeux")
-let socket = new WebSocket("ws://192.168.1.27:5678/broadcast/write");
->>>>>>> Stashed changes
-
-    ws.on("close", function() {
-        console.log("Browser gone.")
-    })
-});
+socket.onmessage = function (event) {
+	console.log('test')
+	console.log(event.data)
+}
